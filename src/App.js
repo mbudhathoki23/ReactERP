282bd7c41a4e4f6d0009bb68e3bcaabff3086a01
mbudhathoki23 <50546974+mbudhathoki23@users.noreply.github.com>
2023-03-13 12:45:45 +0545

    added assets folder and react bootstrap installation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div>
-       <h1>HELLO</h1>
+      <h1></h1>
     </div>
   );
 }
---
+++ src/components/softwares/payroll/SideNavigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideNavigation = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default SideNavigation
