2dc3a2a17bc71b1bd629150caf3263402e458938
saazan14 <iamsajansubba@gmail.com>
2023-03-13 15:37:20 +0545

    Completed Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import NavsBars from "./components/NavsBars";
-
+import './index.css';
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-=======
     <NavsBars />
->>>>>>> 7ac5cf5f0f99ca74835ebf3a7056c290f6423a98
+
     </div>
   );
 }
