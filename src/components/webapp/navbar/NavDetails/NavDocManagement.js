9a6c7e7a3562a2529c3395d1dbfbe5ba4db148c8
mbudhathoki23 <50546974+mbudhathoki23@users.noreply.github.com>
2023-04-27 18:09:50 +0545

    Navbar MASTER done

--- src/components/webapp/navbar/NavDetails/NavDocManagement.js
+++ src/components/webapp/navbar/NavDetails/NavDocManagement.js
@@ -1,19 +1,89 @@
-export const OtherSolutions = [
-    "Payroll Software",
-    "Sale Force Automation",
-    "Billing Software",
-    "GST Software",
-    "Inventory Software",
-    "Invoice Software",
-    "Accounting Software",
-    "Solution for CA",
-    "Counter Software",
-    "e-Invoicing Software",
-  ];
+export const ChartsofAccounts = [
+  "Account Group",
+  "Account Sub Group",
+  "General Ledger",
+  "Subledger",
+  "Ledger Import",
+  "Merge Ledger",
+  "Join Ledger",
+  "Ledger Lock",
+];
+
+export const AgentMaster = ["Main Agent", "Agent"];
+
+export const AreaMaster = ["Main Agent", "Agent"];
+
+export const MemberShipSetup = [
+  "      MemberShip Type",
+  "     MemberShip setup",
+  "Gift Voucher Generate",
+];
+
+export const DepartmentMASTER
+= [
+  "Department" 
+
+];
+
+
+export const TerminalMaster
+= [
+  "Terminal Setup",
+  "Floor Setup",
+  "RoomNo Setup",
+  "TableNO setup",
+];
+
+
+
+export const CurrencyMaster = [
+  "Currency",
   
-  export const ERPsolutions = [
-    "Retail Chain Management",
-    "Supply Chain Management",
-    "ERP Software Solution",
-    "Pharma ERP Software",
-  ];
+];
+
+
+
+export const CostCenterMaster
+= [
+  "CostCenter"
+];
+
+
+export const ProductMASTER
+= [
+  "Product SubGroup",
+"Product Unit",
+"Product/Item/Inventory",
+"Counter Product/Barcode",
+"(Mini) Counter Product/Barcode",
+"Spare Parts / Raw Materials",
+"Menu Setup",
+"Sub Menu Item",
+"Restro Product/Menu Items",
+"Movies",
+"Product Scheme",
+"Product Mapping",
+"Product Lock",
+"Product Update",
+"Product Import",
+"Barcode Print",
+"Product Assemble",
+"Spare Parts Import",
+];
+
+export const GodownMaster
+
+= [
+ " Godown",
+ " Warehouse",
+  "Rack Setup",
+];
+
+export const NarrationMaster
+
+= [
+ " Narration & Remarks",
+];
+
+
+// export const UserDefinedField
